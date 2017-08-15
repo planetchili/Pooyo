0c55ff90203ae4116a568679dade856fd3e88517
planetchili <planetchili@users.noreply.github.com>
2017-08-15 23:09:33 +0900

    cleanup graphics

--- Engine/Graphics.cpp
+++ Engine/Graphics.cpp
@@ -26,17 +26,10 @@
 #include <string>
 #include <array>
 
-// Ignore the intellisense error "cannot open source file" for .shh files.
-// They will be created during the build sequence before the preprocessor runs.
-namespace FramebufferShaders
-{
-#include "FramebufferPS.shh"
-#include "FramebufferVS.shh"
-}
-
 #pragma comment( lib,"d3d11.lib" )
 
 using Microsoft::WRL::ComPtr;
+namespace dx = DirectX;
 
 Graphics::Graphics( HWNDKey& key )
 {
@@ -101,12 +94,10 @@ Graphics::Graphics( HWNDKey& key )
 	{
 		throw CHILI_GFX_EXCEPTION( hr,L"Creating render target view on backbuffer" );
 	}
-
-
+	
 	// set backbuffer as the render target using created view
 	pImmediateContext->OMSetRenderTargets( 1,pRenderTargetView.GetAddressOf(),nullptr );
-
-
+	
 	// set viewport dimensions
 	D3D11_VIEWPORT vp;
 	vp.Width = float( Graphics::ScreenWidth );
@@ -116,176 +107,12 @@ Graphics::Graphics( HWNDKey& key )
 	vp.TopLeftX = 0.0f;
 	vp.TopLeftY = 0.0f;
 	pImmediateContext->RSSetViewports( 1,&vp );
-
-
-	///////////////////////////////////////
-	// create texture for cpu render target
-	D3D11_TEXTURE2D_DESC sysTexDesc;
-	sysTexDesc.Width = Graphics::ScreenWidth;
-	sysTexDesc.Height = Graphics::ScreenHeight;
-	sysTexDesc.MipLevels = 1;
-	sysTexDesc.ArraySize = 1;
-	sysTexDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
-	sysTexDesc.SampleDesc.Count = 1;
-	sysTexDesc.SampleDesc.Quality = 0;
-	sysTexDesc.Usage = D3D11_USAGE_DYNAMIC;
-	sysTexDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
-	sysTexDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
-	sysTexDesc.MiscFlags = 0;
-	// create the texture
-	if( FAILED( hr = pDevice->CreateTexture2D( &sysTexDesc,nullptr,&pSysBufferTexture ) ) )
-	{
-		throw CHILI_GFX_EXCEPTION( hr,L"Creating sysbuffer texture" );
-	}
-
-	D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
-	srvDesc.Format = sysTexDesc.Format;
-	srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
-	srvDesc.Texture2D.MipLevels = 1;
-	// create the resource view on the texture
-	if( FAILED( hr = pDevice->CreateShaderResourceView( pSysBufferTexture.Get(),
-		&srvDesc,&pSysBufferTextureView ) ) )
-	{
-		throw CHILI_GFX_EXCEPTION( hr,L"Creating view on sysBuffer texture" );
-	}
-
-
-	////////////////////////////////////////////////
-	// create pixel shader for framebuffer
-	// Ignore the intellisense error "namespace has no member"
-	if( FAILED( hr = pDevice->CreatePixelShader(
-		FramebufferShaders::FramebufferPSBytecode,
-		sizeof( FramebufferShaders::FramebufferPSBytecode ),
-		nullptr,
-		&pPixelShader ) ) )
-	{
-		throw CHILI_GFX_EXCEPTION( hr,L"Creating pixel shader" );
-	}
-	
-
-	/////////////////////////////////////////////////
-	// create vertex shader for framebuffer
-	// Ignore the intellisense error "namespace has no member"
-	if( FAILED( hr = pDevice->CreateVertexShader(
-		FramebufferShaders::FramebufferVSBytecode,
-		sizeof( FramebufferShaders::FramebufferVSBytecode ),
-		nullptr,
-		&pVertexShader ) ) )
-	{
-		throw CHILI_GFX_EXCEPTION( hr,L"Creating vertex shader" );
-	}
-	
-
-	//////////////////////////////////////////////////////////////
-	// create and fill vertex buffer with quad for rendering frame
-	const FSQVertex vertices[] =
-	{
-		{ -1.0f,1.0f,0.5f,0.0f,0.0f },
-		{ 1.0f,1.0f,0.5f,1.0f,0.0f },
-		{ 1.0f,-1.0f,0.5f,1.0f,1.0f },
-		{ -1.0f,1.0f,0.5f,0.0f,0.0f },
-		{ 1.0f,-1.0f,0.5f,1.0f,1.0f },
-		{ -1.0f,-1.0f,0.5f,0.0f,1.0f },
-	};
-	D3D11_BUFFER_DESC bd = {};
-	bd.Usage = D3D11_USAGE_DEFAULT;
-	bd.ByteWidth = sizeof( FSQVertex ) * 6;
-	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-	bd.CPUAccessFlags = 0u;
-	D3D11_SUBRESOURCE_DATA initData = {};
-	initData.pSysMem = vertices;
-	if( FAILED( hr = pDevice->CreateBuffer( &bd,&initData,&pVertexBuffer ) ) )
-	{
-		throw CHILI_GFX_EXCEPTION( hr,L"Creating vertex buffer" );
-	}
-
-	
-	//////////////////////////////////////////
-	// create input layout for fullscreen quad
-	const D3D11_INPUT_ELEMENT_DESC ied[] =
-	{
-		{ "POSITION",0,DXGI_FORMAT_R32G32B32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0 },
-		{ "TEXCOORD",0,DXGI_FORMAT_R32G32_FLOAT,0,12,D3D11_INPUT_PER_VERTEX_DATA,0 }
-	};
-
-	// Ignore the intellisense error "namespace has no member"
-	if( FAILED( hr = pDevice->CreateInputLayout( ied,2,
-		FramebufferShaders::FramebufferVSBytecode,
-		sizeof( FramebufferShaders::FramebufferVSBytecode ),
-		&pInputLayout ) ) )
-	{
-		throw CHILI_GFX_EXCEPTION( hr,L"Creating input layout" );
-	}
-
-
-	////////////////////////////////////////////////////
-	// Create sampler state for fullscreen textured quad
-	D3D11_SAMPLER_DESC sampDesc = {};
-	sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
-	sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
-	sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
-	sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
-	sampDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
-	sampDesc.MinLOD = 0;
-	sampDesc.MaxLOD = D3D11_FLOAT32_MAX;
-	if( FAILED( hr = pDevice->CreateSamplerState( &sampDesc,&pSamplerState ) ) )
-	{
-		throw CHILI_GFX_EXCEPTION( hr,L"Creating sampler state" );
-	}
-
-	// allocate memory for sysbuffer (16-byte aligned for faster access)
-	pSysBuffer = reinterpret_cast<Color*>( 
-		_aligned_malloc( sizeof( Color ) * Graphics::ScreenWidth * Graphics::ScreenHeight,16u ) );
-}
-
-Graphics::~Graphics()
-{
-	// free sysbuffer memory (aligned free)
-	if( pSysBuffer )
-	{
-		_aligned_free( pSysBuffer );
-		pSysBuffer = nullptr;
-	}
-	// clear the state of the device context before destruction
-	if( pImmediateContext ) pImmediateContext->ClearState();
 }
 
 void Graphics::EndFrame()
 {
-	HRESULT hr;
-
-	// lock and map the adapter memory for copying over the sysbuffer
-	if( FAILED( hr = pImmediateContext->Map( pSysBufferTexture.Get(),0u,
-		D3D11_MAP_WRITE_DISCARD,0u,&mappedSysBufferTexture ) ) )
-	{
-		throw CHILI_GFX_EXCEPTION( hr,L"Mapping sysbuffer" );
-	}
-	// setup parameters for copy operation
-	Color* pDst = reinterpret_cast<Color*>(mappedSysBufferTexture.pData );
-	const size_t dstPitch = mappedSysBufferTexture.RowPitch / sizeof( Color );
-	const size_t srcPitch = Graphics::ScreenWidth;
-	const size_t rowBytes = srcPitch * sizeof( Color );
-	// perform the copy line-by-line
-	for( size_t y = 0u; y < Graphics::ScreenHeight; y++ )
-	{
-		memcpy( &pDst[ y * dstPitch ],&pSysBuffer[y * srcPitch],rowBytes );
-	}
-	// release the adapter memory
-	pImmediateContext->Unmap( pSysBufferTexture.Get(),0u );
-
-	// render offscreen scene texture to back buffer
-	pImmediateContext->IASetInputLayout( pInputLayout.Get() );
-	pImmediateContext->VSSetShader( pVertexShader.Get(),nullptr,0u );
-	pImmediateContext->PSSetShader( pPixelShader.Get(),nullptr,0u );
-	pImmediateContext->IASetPrimitiveTopology( D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST );
-	const UINT stride = sizeof( FSQVertex );
-	const UINT offset = 0u;
-	pImmediateContext->IASetVertexBuffers( 0u,1u,pVertexBuffer.GetAddressOf(),&stride,&offset );
-	pImmediateContext->PSSetShaderResources( 0u,1u,pSysBufferTextureView.GetAddressOf() );
-	pImmediateContext->PSSetSamplers( 0u,1u,pSamplerState.GetAddressOf() );
-	pImmediateContext->Draw( 6u,0u );
-
 	// flip back/front buffers
+	HRESULT hr;
 	if( FAILED( hr = pSwapChain->Present( 1u,0u ) ) )
 	{
 		if( hr == DXGI_ERROR_DEVICE_REMOVED )
@@ -301,19 +128,7 @@ void Graphics::EndFrame()
 
 void Graphics::BeginFrame()
 {
-	// clear the sysbuffer
-	memset( pSysBuffer,0u,sizeof( Color ) * Graphics::ScreenHeight * Graphics::ScreenWidth );
 }
-
-void Graphics::PutPixel( int x,int y,Color c )
-{
-	assert( x >= 0 );
-	assert( x < int( Graphics::ScreenWidth ) );
-	assert( y >= 0 );
-	assert( y < int( Graphics::ScreenHeight ) );
-	pSysBuffer[Graphics::ScreenWidth * y + x] = c;
-}
-
 
 //////////////////////////////////////////////////
 //           Graphics Exception
