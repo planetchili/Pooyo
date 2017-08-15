f111de77f048f8a1d7989844ccfeb0410a3278c3
planetchili <planetchili@users.noreply.github.com>
2017-08-16 00:37:58 +0900

    fix potential problem in cominit

--- Engine/COMInitializer.cpp
+++ Engine/COMInitializer.cpp
@@ -5,10 +5,9 @@ size_t COMInitializer::refCount = 0u;
 
 COMInitializer::COMInitializer()
 {
-	if( refCount == 0u )
+	if( refCount++ == 0u )
 	{
 		CoInitializeEx( NULL,COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE );
-		refCount++;
 	}
 }
 
