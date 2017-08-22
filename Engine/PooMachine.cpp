b0fd992f56a10634f4c3043c8c7422b323809ed7
SpaceAnimation <agphysics@live.com>
2017-08-22 20:11:20 +1000

    commit

--- Engine/PooMachine.cpp
+++ Engine/PooMachine.cpp
@@ -1,34 +1,11 @@
 #include "PooMachine.h"
-//#include "PooComponents.h"
-//#include "GameObject.h"
 
 PooMachine::PooMachine()
 {
-	//poo.push_back(createPoo());
+
 }
 
 
 PooMachine::~PooMachine()
 {
 }
-
-void PooMachine::loadSprites()
-{
-	//poo_blue	= &gfx.MakeSprite(L"../Art/Node_Blue.png", { 0,0,12,12 }, 4.0f, { 16.0f,24.0f });
-	//poo_green	= &gfx.MakeSprite(L"../Art/Node_Green.png", { 0,0,12,12 }, 4.0f, { 28.0f,24.0f });
-	//poo_purple	= &gfx.MakeSprite(L"../Art/Node_Purple.png", { 0,0,12,12 }, 4.0f, { 40.0f,24.0f });
-	//poo_Red		= &gfx.MakeSprite(L"../Art/Node_Red.png", { 0,0,12,12 }, 4.0f, { 52.0f,24.0f });
-}
-
-void PooMachine::update()
-{
-	//for (auto p : poo)
-	//{
-	//	p->update();
-	//}
-}
-
-//GameObject* PooMachine::createPoo()
-//{
-//	return new GameObject(new PooInputComponent(), new PooPhysicsComponent(), new PooGraphicsComponent());
-//}
--- Engine/PooMachine.h
+++ Engine/PooMachine.h
@@ -1,28 +1,12 @@
 #pragma once
 
-//#include <vector>
-//#include "GameObject.h"
-//class GameObject;
-
-
 class PooMachine
 {
 public:
+
 	PooMachine();
 	~PooMachine();
-
-	void loadSprites();
-	void update();
-
-private:
-	//GameObject* createPoo();
-	
-	//std::vector<GameObject*> poo;
-
-	/*****Sprites********/
-	//Sprite* poo_blue;
-	//Sprite* poo_green;
-	//Sprite* poo_purple;
-	//Sprite* poo_Red;
+public:
+	int dummyVar;
 };
 
