7519e7e7a9230ae8cf3e044f7ab7054fdebcb83f
SpaceAnimation <agphysics@live.com>
2017-08-29 20:35:04 +1000

    removed custom Vec2
    Now using DirectX::SimpleMath::Vector2
    -seeing DirectX already has a vector2 class there is no need to create a
    custom one which isnt doing anything different

--- Engine/GameObject.h
+++ Engine/GameObject.h
@@ -7,30 +7,8 @@ using namespace DirectX::SimpleMath;
 class GameObject
 {
 public:
-	//struct Vec2
-	//{
-	//	float x;
-	//	float y;
-	//	Vec2() { x = 0.0f; y = 0.0f; }
-	//	Vec2(float x, float y)
-	//	{
-	//		this->x = x; this->y = y;
-	//	}
-	//	Vec2& operator+=(Vec2 const& rhs)
-	//	{
-	//		this->x += rhs.x;
-	//		this->y += rhs.y;
-	//		return *this;
-	//	}
-	//	Vec2 operator*(float const& rhs)
-	//	{
-	//		return Vec2(this->x * rhs, this->y *= rhs);
-	//	}
-	//};
-
 	float diameter = 12.0f * 4.0f;
 	float speed = 20.0f;
-	//Vec2 position;
 	Vector2 position;
 
 	GameObject(ComponentInput*, ComponentPhysics*, ComponentGraphics*);
